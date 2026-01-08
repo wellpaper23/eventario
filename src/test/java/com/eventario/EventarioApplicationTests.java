195b70c484ae46b92064f8966af58eebb75f8e43
wellpaper23 <wevelle98@gmail.com>
2026-01-08 14:48:26 -0300

    First Commit

---
+++ src/test/java/com/eventario/EventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
