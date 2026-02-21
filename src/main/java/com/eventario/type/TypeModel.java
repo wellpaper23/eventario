057233be5729fab8a226677f01e1892ae3f0d2e7
wellpaper23 <wevelle98@gmail.com>
2026-02-21 16:24:23 -0300

    feat: add type model entity

--- src/main/java/com/eventario/type/TypeModel.java
+++ src/main/java/com/eventario/type/TypeModel.java
@@ -2,29 +2,37 @@ package com.eventario.type;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Generated;
 
 @Data
-@AllArgsConstructor
+
 @NoArgsConstructor
 
 @Entity
 @Table(name = "type")
 public class TypeModel {
+    public TypeModel(String name, String description) {
+        this.uuid = UUID.randomUUID();
+        this.name = name;
+        this.description = description;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_type")
-    long id;
+    Long id;
 
-    @Column(name = "uuid_type", unique = true)
+    @Column(name = "uuid_type", unique = true, updatable = false,nullable = false)
     UUID uuid;
 
     @Column(name = "name")
     String name;
+
     @Column(name = "description")
     String description;
 
