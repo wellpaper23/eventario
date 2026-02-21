a6dfa946e0fde300410c88213e1c180f24903484
wellpaper23 <wevelle98@gmail.com>
2026-02-21 16:32:40 -0300

    feat: add typeServices class with CRUD commands

---
+++ src/main/java/com/eventario/type/TypeServices.java
@@ -0,0 +1,45 @@
+package com.eventario.type;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+
+public class TypeServices {
+
+    private final TypeRepository typeRepository;
+
+    public TypeServices(TypeRepository typeRepository) {
+        this.typeRepository = typeRepository;
+    }
+
+    // find type by id
+    TypeModel showTypeById(long id) {
+        Optional<TypeModel> typeModel = typeRepository.findById(id);
+        return typeModel.orElse(null);
+    }
+
+    // create types
+    TypeModel createType(TypeModel typeModel) {
+        if (typeModel.getId() != null) return null;
+        return typeRepository.save(typeModel);
+    }
+
+    // list all types
+    List<TypeModel> listAllTypes() {
+        return typeRepository.findAll();
+    }
+
+    // delete types
+    void deleteType(long id) {
+        typeRepository.deleteById(id);
+    }
+
+    // update types
+    TypeModel updateType(long id, TypeModel typeModel){
+             return typeRepository.save(typeModel);
+         }
+     }
