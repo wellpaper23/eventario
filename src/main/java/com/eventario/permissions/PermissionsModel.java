b089a792becc1e9ef0c54ea30821e00b7d0f0bb3
wellpaper23 <wevelle98@gmail.com>
2026-02-27 19:34:49 -0300

    feat: add PermissionsModel entity

---
+++ src/main/java/com/eventario/permissions/PermissionsModel.java
@@ -0,0 +1,36 @@
+package com.eventario.permissions;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "permissions")
+
+@Data
+@NoArgsConstructor
+public class PermissionsModel {
+
+    public PermissionsModel(int id, String uuid, String description, String typePermission, String name) {
+        this.id = id;
+        this.uuid = UUID.randomUUID();
+        this.description = description;
+        this.typePermission = typePermission;
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(name = "uuid", unique = true, updatable = false,nullable = false)
+    UUID uuid;
+    @Column(name = "description")
+    String description;
+    @Column(name = "type_permission")
+    String typePermission;
+    @Column(name = "name")
+    String name;
+
+}
